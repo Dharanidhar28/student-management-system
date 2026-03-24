7fb0dbf65884920108f23324f0f7151c11e50121
Dharanidhar28 <dharanidharkotha@gmail.com>
2026-03-24 23:27:58 +0530

    modified database file

--- backend/app/database.py
+++ backend/app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from sqlalchemy.orm import Session
 
-DATABASE_URL = "sqlite:///students.db"
+DATABASE_URL = "sqlite:///./students.db"
 
 engine = create_engine(DATABASE_URL)
 
