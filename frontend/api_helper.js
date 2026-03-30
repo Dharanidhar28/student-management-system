f17a38d8d41d7b13aa2943d5f79952d9baac3b28
Dharanidhar28 <dharanidharkotha@gmail.com>
2026-03-31 00:43:34 +0530

    Force frontend to use Render backend URL

--- frontend/api_helper.js
+++ frontend/api_helper.js
@@ -1,6 +1,8 @@
 export async function apiRequest(endpoint, options = {}) {
 	const token = localStorage.getItem("token");
-	const res = await fetch(window.APP_CONFIG.getApiBase() + endpoint, {
+	const apiBase =
+		window.APP_CONFIG?.DEFAULT_API_BASE || window.APP_CONFIG.getApiBase();
+	const res = await fetch(apiBase + endpoint, {
 		...options,
 
 		headers: {
