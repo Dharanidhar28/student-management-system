c23dc2cc5b73cea5279d9217de08ab8a81e7a575
Dharanidhar28 <dharanidharkotha@gmail.com>
2026-03-31 00:48:04 +0530

    Revert "Remove cached frontend API override"
    This reverts commit 0d6f275f9cb3d11295eed5f81656cd0996a08669.

--- frontend/config.js
+++ frontend/config.js
@@ -1,7 +1,8 @@
 const DEFAULT_API_BASE = "https://student-management-system-qdeq.onrender.com";
 
 function getApiBase() {
-	const configuredBase = window.APP_API_BASE_URL;
+	const configuredBase =
+		window.APP_API_BASE_URL || window.localStorage.getItem("apiBaseUrl");
 	if (configuredBase) {
 		return configuredBase.replace(/\/$/, "");
 	}
