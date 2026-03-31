a19297a4c6e5b9deb42fcbc2ae335e02929af521
Dharanidhar28 <dharanidharkotha@gmail.com>
2026-03-31 23:00:47 +0530

    serve frontend files

--- backend/app/main.py
+++ backend/app/main.py
@@ -23,11 +23,11 @@ app.add_middleware(
 )
 
 
-app.mount("/static", StaticFiles(directory="../../frontend"), name="static")
+app.mount("/frontend", StaticFiles(directory="frontend"), name="frontend")
 
 @app.get("/")
 def read_root():
-    return FileResponse("../../frontend/login.html")
+    return FileResponse("/frontend/login.html")
 
 
 import os
